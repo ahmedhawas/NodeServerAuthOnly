06bfaeefecdd3ceb05224e66669bd1fff254014d
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-05-01 11:23:24 -0400

    Add passport

--- models/user.js
+++ models/user.js
@@ -23,6 +23,13 @@ userSchema.pre('save', function(next) { // before user gets saved
   });
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
+    callback(null, isMatch);
+  });
+}
+
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema); // all users
 
