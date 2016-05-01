42111feea1968f369b9e82ce8532ed409e068433
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-05-01 13:38:04 -0400

    allow cors request

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined')); // setup as middleware - logging framework
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' })); // parse any request as a JSON
 router(app); //call the router with our app
 
--- router.js
+++ router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', {session: false});
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
-    res.send({hi: 'there'});
+    res.send({ message: 'Some Secret code or something' });
   });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
