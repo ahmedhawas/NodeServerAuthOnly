fa9685b7b855305bf5378d7c408e506c901f8a6f
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-05-01 03:39:24 -0400

    added controller and signup functionality with bcrypt for password

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt-nodejs');
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowerCase: true },
   password: String
+});
+
+// on save hoook bcrypt password
+userSchema.pre('save', function(next) { // before user gets saved
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the model class
