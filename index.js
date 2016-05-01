b513a00bdd633725f8097b850317dc28c0b6be73
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-05-01 00:11:18 -0400

    Added user model and get route for test route

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
+
 // App Setup
+app.use(morgan('combined')); // setup as middleware - logging framework
+app.use(bodyParser.json({ type: '*/*' })); // parse any request as a JSON
+router(app); //call the router with our app
 
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 
 server.listen(port);
-console.log('Server listening to port:', port)
+console.log('Server listening to port:', port);
